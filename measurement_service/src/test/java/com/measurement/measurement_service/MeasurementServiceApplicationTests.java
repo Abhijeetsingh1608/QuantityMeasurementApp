257851f6ec9580bac733a81ed612cc793c9122f4
Abhijeetsingh1608 <kumarabhisingh2020@gmail.com>
2026-04-13 14:28:33 +0530

    [Abhijeet]: Added microservices feature

---
+++ measurement_service/src/test/java/com/measurement/measurement_service/MeasurementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.measurement.measurement_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasurementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
